08fd97d6df406517e5b514e0da3989f8d38ffc9d
Irati Kintana <145665879+quintanairati@users.noreply.github.com>
2023-12-12 12:30:38 +0000

    commit3

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,39 @@
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+var path = require('path');
+const ruta = path.join(__dirname, '../public/uploads');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, ruta)
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.originalname)
+    },
+    fileSize: function (req, file, cb) {
+      cb(null, file.size)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+  const fitx = file.originalname.split('.');
+  const mota = fitx[fitx.length - 1];
+  if ( mota === 'jpg' || mota === 'png' || mota === 'PNG' || mota === 'JPG') {
+    cb(null, true);
+  } else {
+    cb(new Error('Ez da PNG edo JPG motako fitxategia'), false);
+  }
+};
+  
+const upload = multer({ storage: storage,
+                        limits: {
+                          fileSize: 2 * 1024 *1024,
+                        },
+                        fileFilter: fileFilter,
+                      })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
